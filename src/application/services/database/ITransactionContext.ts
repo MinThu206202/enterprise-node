96b9265825f6cc35d927e7126cf07905a83d3e3c
Min Thu <kitkit206202@gmail.com>
2026-08-19 10:17:28 +0630

    assign default User role when the register

--- src/application/services/database/ITransactionContext.ts
+++ src/application/services/database/ITransactionContext.ts
@@ -1,7 +1,11 @@
 import { IOutboxRepository } from "../../../domain/repositories/IOutboxRepository.js";
+import { IRoleRepository } from "../../../domain/repositories/IRoleRepository.js";
 import { IUserRepository } from "../../../domain/repositories/IUserRepository.js";
+import { IUserRoleRepository } from "../../../domain/repositories/IUserRoleRepository.js";
 
 export interface ITransactionContext{
     userRepository : IUserRepository,
-    outboxRepository : IOutboxRepository
+    outboxRepository : IOutboxRepository,
+    roleRepository : IRoleRepository,
+    userRoleRepository : IUserRoleRepository
 }
